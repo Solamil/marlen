967391657ce36d690f838ab61c64a2ccd6b5830a
Michal Kukla <michal.kukla@tul.cz>
2022-11-24 20:03:37 +0100

    Added link parameters wttr widget link

--- web/js/module-wttrin-widget.js
+++ web/js/module-wttrin-widget.js
@@ -1,11 +1,14 @@
 function changeWeather () {
 	var input = document.getElementById('weather-input');
 	var img = document.getElementById('weather-img');
+	var wttrLink = document.getElementById("wttr-link")
 	img.src =
 	'https://wttr.in/' + input.value +
 	'_' + weatherOptions
 	+ '.png';
 	img.alt = weatherImgAlt + input.value;
+	wttrLink.href = 'https://wttr.in/' + input.value +
+	'?lang='+langOption;
 	input.value = '';
 }
 
@@ -18,6 +21,13 @@ document.getElementById('weather-form').onkeypress = function(event) {
 	  event.preventDefault();
 	  changeWeather();
 	}
+}
+var langOption = "";
+var cookieList = document.cookie.split("; ");
+var cookieLang = cookieList.find(e => e.startsWith("lang"));
+if (cookieLang != undefined) {
+	lang = cookieLang.split("=")[1];
+	langOption = lang.split("-")[0];
 }
 
 var weatherOptions = '0pq_transparency=255_background=893531_lang=en';
