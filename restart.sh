3505cb28e4b63e940882f9c018885ae19a88aafa
Michal Kukla <michal.kukla@tul.cz>
2022-12-10 15:11:51 +0100

    Removed unnecessary copying and kill only user's program

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export GOROOT=/usr/local/go
 export PATH=$GOROOT/bin:"$PATH"
-pgrep main && kill -2 "$(pgrep main)"
+kill -2 "$(pgrep main -u "$(whoami)")" 2>&1
 setsid -f go run ./main.go >/dev/null 2>&1
