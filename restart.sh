12d77cbf9a3b12be390f1ed3b8d4bbc8603aff73
Michal Kukla <michal.kukla@tul.cz>
2023-01-07 20:04:12 +0100

    Updated .gitignore, store the errors when app run

--- restart.sh
+++ restart.sh
@@ -5,4 +5,4 @@ export PATH=$GOROOT/bin:"$PATH"
 go test -coverprofile cover.out ./main.go ./main_test.go
 go tool cover -html=cover.out -o cover.html
 kill -2 "$(pgrep main -u "$(whoami)")" 2>&1
-setsid -f go run ./main.go >/dev/null 2>&1
+setsid -f go run ./main.go >> errors.log 
