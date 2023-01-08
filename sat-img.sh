3703b71e4f77bff021fb6ff1b6424517f8f3707a
Michal Kukla <michal.kukla@tul.cz>
2023-01-08 11:40:10 +0100

    Links into variables, set connect-timeout

--- sat-img.sh
+++ sat-img.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-repo_path="$HOME/repo"
+#repo_path="$HOME/repo"
+repo_path="$HOME/.local/src/marlen"
 repo_pics_path=$repo_path"/web/pics"
-curl --parallel --parallel-max 5 \
-	https://api.sat24.com/mostrecent/DE/visual5hdcomplete --output "$repo_pics_path"/clouds.png \
-	https://api.sat24.com/mostrecent/DE/rainTMC --output "$repo_pics_path"/rain.png \
-	https://api.sat24.com/animated/DE/rainTMC/3/Central%20Europe%20Standard%20Time/5603172 --output "$repo_pics_path"/rain.gif \
-	https://api.sat24.com/animated/DE/visual/3/Central%20Europe%20Standard%20Time/530005 --output  "$repo_pics_path"/clouds.gif
+the_server="https://api.sat24.com"
+pic_link="${the_server}/mostrecent/DE"
+gif_link="${the_server}/animated/DE"
+time="Central%20Europe%20Standard%20Time"
+
+curl --connect-timeout 5 --parallel --parallel-max 5 \
+	${pic_link}/visual5hdcomplete --output "$repo_pics_path"/clouds.png \
+	${pic_link}/rainTMC --output "$repo_pics_path"/rain.png \
+	${gif_link}/rainTMC/3/${time}/5603172 --output "$repo_pics_path"/rain.gif \
+	${gif_link}/visual/3/${time}/530005 --output  "$repo_pics_path"/clouds.gif
 
 
 cwebp -resize 400 300 "$repo_pics_path"/rain.png -o "$repo_pics_path"/rain.webp &
