39f9edf0fee648a60a9f165dd903b1fb48a29f6d
Michal Kukla <michal.kukla@tul.cz>
2023-01-12 11:52:17 +0100

    Gif is better than html page.

---
+++ days-forecast.sh
@@ -0,0 +1,32 @@
+# today=$(date -d +"%Y%m%d")
+# hour=$(date -d +"%H")
+
+t="tomorrow"
+tomorrow=$(date -d "$t" "+%Y%m%d")
+formatted=$(date -d "$t" "+%d.%m. %A")
+forecastType=${1:-"forecastPrecip"}
+continent=${2:-"europa"}
+url="https://en.sat24.com/image?type=${forecastType}&region=${continent}&timestamp="$tomorrow
+prefix="img"
+img_type="png"
+#dir="repo/web/pics"
+dir="web/pics"
+dir_images="${dir}/sat24"
+out="${dir}/forecast_tmrw.gif"
+[ -d $dir_images ] || mkdir $dir_images
+
+for i in "00" "03" "06" "09" "12" "15" "18" "21"; do
+	clock="${i}00"
+	pic="${dir_images}/${prefix}${tomorrow}${clock}.${img_type}"
+	if [ ! -f "$pic" ]; then
+		curl "${url}${clock}"  --output "$pic"
+		convert "$pic" -gravity South -pointsize 18 -undercolor white -fill black  -annotate +0+0 "${formatted} ${i}:00" "$pic"
+	fi
+done 
+
+convert -delay 160 "${dir_images}/${prefix}${tomorrow}*${img_type}" "$out"
+
+cwebp -resize 300 400 "${dir_images}/${prefix}${tomorrow}0000.${img_type}" -o "$dir/forecast_tmrw.webp"
+rm ${dir_images}/*png
+
+# convert img0000.png -gravity South -pointsize 18 -fill yellow -annotate +0+0 "16.12.2022 00:00" img0001.png
--- main.go
+++ main.go
@@ -90,20 +90,11 @@ func main() {
 	http.HandleFunc("/pics/forecast_tmrw.webp", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "web/pics/forecast_tmrw.webp")
 	})
-	http.HandleFunc("/pics/legend-forecastPrecip.webp", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/pics/legend-forecastPrecip.webp")
-	})
-	http.HandleFunc("/pics/legend-forecastTemp.webp", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/pics/legend-forecastTemp.webp")
+	http.HandleFunc("/pics/forecast_tmrw.gif", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web/pics/forecast_tmrw.gif")
 	})
 	http.HandleFunc("/js/module-wttrin-widget.js", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "web/js/module-wttrin-widget.js")
-	})
-	http.HandleFunc("/forecast.html", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/forecast.html")
-	})
-	http.HandleFunc("/forecast1.html", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/forecast1.html")
 	})
 	http.HandleFunc("/cover.html", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "cover.html")
