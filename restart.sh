ef71064da5be036f67182e56d17dceaf1abd557e
Michal Kukla <michal.kukla@tul.cz>
2023-03-21 14:42:17 +0100

    restart.sh: No exit status 1

--- restart.sh
+++ restart.sh
@@ -3,9 +3,9 @@
 export GOROOT=/usr/local/go
 export PATH=$GOROOT/bin:"$PATH"
 web_dir="web/"
-go test -coverprofile cover.out ./main.go ./main_test.go || exit 1
+go test -coverprofile cover.out ./main.go ./main_test.go
 go tool cover -html=cover.out -o ${web_dir}cover.html
 printf "Closing old instance: %d" "$(pgrep main -u "$(whoami)")"
 kill -2 "$(pgrep main -u "$(whoami)")" 2>&1
-setsid -f go run ./main.go >/dev/null 2>&1 || exit 1
+setsid -f go run ./main.go >/dev/null 2>&1
 printf "Started new instance: %d" "$(pgrep main -u "$(whoami)")"
