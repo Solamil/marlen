9fa41a7f8dc088ca06bae45bd5eb834a3b84fdd8
Michal Kukla <michal.kukla@tul.cz>
2023-03-23 15:39:47 +0100

    sb-forecast.sh: added option max-time

--- sb-forecast.sh
+++ sb-forecast.sh
@@ -4,7 +4,7 @@ weatherreport="./weatherreport"
 line_temp=${2:-13}
 line_hum=${3:-16}
 place=$(echo "$place" | sed "s/ /%20/" )
-curl --connect-timeout 2 "$url" > $weatherreport
+curl --connect-timeout 2 --max-time 5 "$url" > $weatherreport
 
 printf "%s" "$(sed "${line_hum}q;d" "$weatherreport" |
 	grep -wo "[0-9]*%" | sort -rn | sed "s/^/☔/g;1q" | tr -d '\n')"
