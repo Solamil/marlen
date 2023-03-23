0a82160b1cdc4c784fad96682d2e1411186433a7
Michal Kukla <michal.kukla@tul.cz>
2023-03-23 09:19:19 +0100

    restart.sh: More ordinary go test command

--- restart.sh
+++ restart.sh
@@ -3,7 +3,7 @@
 export GOROOT=/usr/local/go
 export PATH=$GOROOT/bin:"$PATH"
 web_dir="web/"
-go test -coverprofile cover.out ./main.go ./main_test.go
+go test -v -coverprofile ./...
 go tool cover -html=cover.out -o ${web_dir}cover.html
 printf "Closing old instance: %d" "$(pgrep main -u "$(whoami)")"
 kill -2 "$(pgrep main -u "$(whoami)")" 2>&1
